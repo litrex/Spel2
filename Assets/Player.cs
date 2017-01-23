5458bb915dd84720cfd0828b6d1455d2e86377a9
BigDickOls <litrex1@hotmail.com>
2017-01-23 14:03:21 +0100

    no message

---
+++ Assets/Player.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public int deathtimer;
+	public float speed;
+	bool grounded = true;
+	public float jump;
+	public bool isLookingRight = true;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Ground")
+		{
+			grounded = true;	
+		}
+		if (coll.gameObject.tag == "Player")
+		{
+			grounded =true;
+		}
+		if (coll.gameObject.tag == "SpikeTrap")
+		{
+			Application.LoadLevel(0);
+		}
+		if (coll.gameObject.tag == "LavaPit")
+		{
+			Application.LoadLevel(0);
+		}
+		if (coll.gameObject.tag == "SpikeBlock")
+		{
+			Application.LoadLevel(0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.W) && grounded == true)
+		{
+			rb.AddForce(Vector2.up * jump);
+
+			grounded = false;
+		}
+
+		float speedX = Input.GetAxis("Horizontal");
+		float speedY = Input.GetAxis("Vertical");
+
+		rb.velocity = new Vector2(speedX * speed, rb.velocity.y);
+
+	/*	if(rb.velocity.x == 0)
+		{
+			deathtimer--;
+		}
+
+		if(deathtimer <= 0)
+		{
+			Destroy(gameObject);
+		}
+	*/	
+		if (speedX > 0)
+		{
+			transform.localEulerAngles = new Vector3(0, 180, 0);
+			isLookingRight = false;
+		}
+
+		if (speedX < 0)
+		{
+			transform.localEulerAngles = new Vector3(0, 0, 0);
+			isLookingRight = true;
+		}
+	}
+
+}
---
+++ Assets/Spikeblock.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spikeblock : MonoBehaviour 
+{
+	public float yPos1;
+	public float yPos2;
+	public Rigidbody2D rb;
+	private Vector3 movement = Vector3.up * 0.05f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(transform.position.y > yPos1)
+		{
+			movement = Vector3.down * 0.05f;
+		}
+		if(transform.position.y < yPos2)
+		{
+			movement = Vector3.up * 0.05f;
+		}
+		transform.Translate(movement);
+	}
+}
